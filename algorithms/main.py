9da4cd140b37f14b6152675093a05bea36c83d48
Jon Ander Oribe <jonan.oribe@gmail.com>
2024-11-17 08:09:00 +0100

    Update

--- algorithms/main.py
+++ algorithms/main.py
@@ -1,20 +1,42 @@
-import requests
+from http.client import HTTPException
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from src.k_means_cultura import *
+from src.k_means_deporte import *
+import uvicorn
 
-def interact_with_api_over_multipart_zip(section:str,bearer,zipfile):
-    API_CONNECTION_STRING = 'http://localhost:8000/api/v1.1'
-    headers = {
-        "Authorization": "Bearer " + bearer
-          }
-    response = requests.post(API_CONNECTION_STRING+section, files={'file': zipfile}, headers=headers)
-    if response.text != 'true':
-        print(response.text)
-    elif response.text != 'true' and response.text != True:
-        print(response.text)
-    return response
+# Initialize the FastAPI app
+app = FastAPI()
 
+# Define a route for clustering
+@app.get("/cluster-cultura/")
+async def cluster_cultura():
+    try:
+        try:
+            response = generate_cluster_cultura()
+        except HTTPException:
+            raise HTTPException(status_code=404, detail="Error getting info")
+        return response
+
+    except Exception as e:
+        return JSONResponse({"error": str(e)}, status_code=500)
+
+@app.get("/cluster-deporte/")
+async def cluster_deporte():
+    try:
+        try:
+            response = generate_cluster_cultura()
+        except HTTPException:
+            raise HTTPException(status_code=404, detail="Error getting info")
+        return response
+
+    except Exception as e:
+        return JSONResponse({"error": str(e)}, status_code=500)
+    
 if __name__ == "__main__":
-     bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJUZXN0IiwiZXhwIjoxNzI5ODQxNDI2fQ.oI20FoWaD3DWE_NFshBDyTNv9pzgkJQAmPyo1Ko8Rdw'
-     zipfile =open('json_files_in_memory.zip','rb')
-     section = '/103w08w05-79w28w05w15/strategyData/uploadZipFile/qallocation_algorithm_generated_primitive_portfolios'
-     interact_with_api_over_multipart_zip(section,bearer,zipfile)
-
+    uvicorn.run(app, host="0.0.0.0", port=8000)
